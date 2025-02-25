2458e192217a5125cfcef526ac8c44d772343534
bchen17 <bchen17@binghamton.edu>
2025-02-25 16:57:12 -0500

    s

--- labs/lab4/lab4-base/task3.c
+++ labs/lab4/lab4-base/task3.c
@@ -15,24 +15,23 @@ int main(int argc, char * argv[]) {
 
     close(read);
 
-    int write = open("result", O_WRONLY | O_CREAT | O_TRUNC);
-
-    dup2(write, 1);
-
-    close(write);
-
     pid_t pid = fork();
 
     if (pid < 0){
         printf("did not work");
     } else if (pid == 0){
         printf("IN CHILD: pid=%ld\n", getpid());
+
+        dup2(write, 1);
+
         execvp(argv[1], &argv[1]);
         printf("Failed execution");
         exit(1);
     } else {
         int status;
         waitpid(pid, &status, 0);
+        dup2(1, write);
+        close(write);
         if(status != 0){
             printf("Command %c does not exist.\n", argv[1]);
         }
