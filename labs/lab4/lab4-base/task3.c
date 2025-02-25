acbe0184b4e0dbdcc1d06f36f9f125ad46bc403f
bchen17 <bchen17@binghamton.edu>
2025-02-25 16:51:48 -0500

    initial t3

--- labs/lab4/lab4-base/task3.c
+++ labs/lab4/lab4-base/task3.c
@@ -3,10 +3,37 @@
 
 #include <unistd.h>
 #include <sys/wait.h>
+#include <fcntl.h> 
 
 #define INPUT_FILE "./input/if1"
 
 int main(int argc, char * argv[]) {
+
+    int read = open(INPUT_FILE, O_RDONLY);
+
+    dup2(0, read);
+
+    int write = open("result.txt", O_WRONLY);
+
+    dup2(1, write);
+
     pid_t pid = fork();
+
+    if (pid < 0){
+        printf("did not work");
+    } else if (pid == 0){
+        printf("IN CHILD: pid=%ld\n", getpid());
+        execvp(argv[1], &argv[1]);
+        printf("Failed execution");
+        exit(1);
+    } else {
+        int status;
+        waitpid(pid, &status, 0);
+        if(status != 0){
+            printf("Command %c does not exist.\n", argv[1]);
+        }
+        printf("In PARENT: successfully awaited child (pid = %ld)\n", pid);
+    }
+
     return 0;
 }
