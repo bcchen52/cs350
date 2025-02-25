79758ec51d256e1bfd809f3aa4d3a962c38422fa
bchen17 <bchen17@binghamton.edu>
2025-02-25 16:44:29 -0500

    task 2

--- labs/lab4/lab4-base/task2.c
+++ labs/lab4/lab4-base/task2.c
@@ -7,5 +7,24 @@
 int main(int argc, char * argv[]) {
     
     pid_t pid = fork();
+
+    if (pid < 0){
+        printf("did not work");
+    } else if (pid == 0){
+        printf("Child %ld", getpid());
+        execvp(argv[1], &argv[1]);
+        printf("Failed execution");
+        exit(1);
+      //exit("execlp");
+      //gettimeofday(&end, NULL);
+      //timeval_subtract(&diff, &end, &start);
+    } else {
+        int status;
+        waitpid(pid, &status, 0);
+        if(status != 0){
+            printf("Command %c does not exist.\n", argv[1]);
+        }
+        printf("Successfully waited for child %ld\n", pid);
+
     return 0;
 }
