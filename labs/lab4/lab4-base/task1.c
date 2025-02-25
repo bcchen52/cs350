a322a8421dba8f73d55bd7d11b667d50b5437273
bchen17 <bchen17@binghamton.edu>
2025-02-25 16:38:23 -0500

    swapped task1 and 3

--- labs/lab4/lab4-base/task1.c
+++ labs/lab4/lab4-base/task1.c
@@ -7,5 +7,19 @@
 int main(void) {
     
     pid_t pid = fork();
+
+    if (pid < 0){
+        printf("did not work");
+    } else if (pid == 0){
+        //in child
+        printf("IN CHILD: pid=%ld\n", getpid());
+        execl("ls", "-l -a");
+        printf("Failed execution");
+        exit(1);
+    } else {
+        int status;
+        waitpid(pid, &status, 0);
+        printf("In PARENT: successfully awaited child (pid = %ld)\n", pid);
+    }  
     return 0;
 }
