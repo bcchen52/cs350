5b8b78e7cb6106f6d78db27404aa58e05565e6ab
bchen17 <bchen17@binghamton.edu>
2025-03-01 15:05:47 -0500

    s

--- proj/proj1/base/square.c
+++ proj/proj1/base/square.c
@@ -29,7 +29,7 @@ int main(int argc, char * argv[]) {
             return 0;  // Invalid character → return 0 or handle error
         }
         num = num * 10 + (*ptr - '0');  // Convert character to integer
-        *ptr++;
+        ptr++;
     }
 
     num= sign * num;
