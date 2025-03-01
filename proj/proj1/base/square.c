870a1ae900e0c379ed759b086163c9fdd1498fea
bchen17 <bchen17@binghamton.edu>
2025-03-01 14:57:07 -0500

    s

--- proj/proj1/base/square.c
+++ proj/proj1/base/square.c
@@ -10,23 +10,13 @@ int main(int argc, char * argv[]) {
     //printf("Enter a number: ");
     //scanf("%d", &num);
     if (argc < 2){
-        user_printf(1, "Include an int");
         exit();
     }
-    
-    char *str = argv[1];
-    char *endptr;
 
-    // Convert to integer and check validity
-    int num = strtol(str, &endptr, 10);
-    if (*endptr != '\0') {
-        user_printf(1, "Invalid input\n");
-        exit();
-    }
 
     // Call system call
-    int result = square(num);
-    user_printf(1, "The square of %d is %d\n", num, result);
+    square(argv[1]);
+    //user_printf(1, "The square of %d is %d\n", num, result);
     //printf("The square of %d is %d\n", num, result);
     exit();
 }
