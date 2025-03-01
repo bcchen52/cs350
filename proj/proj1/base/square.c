e01f2a0a39c1eb4cbfa3fa25547d14825944563d
bchen17 <bchen17@binghamton.edu>
2025-03-01 15:03:52 -0500

    s

--- proj/proj1/base/square.c
+++ proj/proj1/base/square.c
@@ -13,9 +13,28 @@ int main(int argc, char * argv[]) {
         exit();
     }
 
+    int num = 0;
+    int sign = 1;
+
+    // Handle negative numbers
+    if (*argv[1] == '-') {
+        sign = -1;
+        *argv[1]++;
+    }
+
+    // Convert characters to numbers
+    while (*argv[1]) {
+        if (*argv[1] < '0' || *argv[1] > '9') {
+            return 0;  // Invalid character → return 0 or handle error
+        }
+        num = num * 10 + (*argv[1] - '0');  // Convert character to integer
+        *argv[1]++;
+    }
+
+    num= sign * num;
 
     // Call system call
-    square(argv[1]);
+    square(num);
     //user_printf(1, "The square of %d is %d\n", num, result);
     //printf("The square of %d is %d\n", num, result);
     exit();
