855d5314c8acca5ac7253eeb4f0a63f6fb059865
bchen17 <bchen17@binghamton.edu>
2025-03-01 14:13:31 -0500

    s

--- proj/proj1/base/square.c
+++ proj/proj1/base/square.c
@@ -1,15 +1,32 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
+#include <ctype.h>
 #include <stdio.h>
 
 
-int main() {
-    int num;
-    printf("Enter a number: ");
-    scanf("%d", &num);
-
-    int result = square(num);
-    printf("The square of %d is %d\n", num, result);
+int main(int argc, char * argv[]) {
+    
+    //int num;
+    //printf("Enter a number: ");
+    //scanf("%d", &num);
+    if (argc < 2){
+        cprintf("Include an int");
+        exit();
+    }
+    
+    if (argv[1] == '\0'){
+        cprintf("Can't be empty");
+        exit();
+    }
+    while (argv[1]) {
+        if (!isdigit(argv[1])){
+            cprintf("Invalid input");
+            exit();
+        }  
+        argv[1]++;
+    }
+    int result = square(argv[1]);
+    //printf("The square of %d is %d\n", num, result);
     exit();
 }
