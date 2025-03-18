1f253c497b394b158bb0048f7a2564e9fc481d9f
bchen17 <bchen17@binghamton.edu>
2025-03-18 15:25:13 -0400

    5

---
+++ labs/lab5/lab5-base/task1.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(void) {
+    char * argv1[] = {"cat", "Makefile", 0};
+    char * argv2[] = {"head", "-4", 0};
+    //char * argv2[] = {"wc", "-l", 0};
+    
+    setbuf(stdout, NULL);
+    
+    int pipefd[2];
+
+    if (pipe(pipefd) == -1) {
+        perror("pipe failed");
+        exit(EXIT_FAILURE);
+    }
+
+
+    pid_t pid_a = fork();
+
+    if (pid_a < 0){
+        printf("did not work");
+    } else if (pid_a == 0){
+        //in child
+        close(pipefd[0]);               
+        dup2(pipefd[1], STDOUT_FILENO);  // Redirect stdout to pipe
+        close(pipefd[1]);    
+        printf("IN CHILD: pid=%ld\n", getpid());
+        execvp(argv2, (char*)0);
+        printf("Failed execution");
+        exit(1);
+    } else {
+        pid_t pid_b = fork();
+
+        if (pid_b == 0){
+            //in child
+            close(pipefd[1]);               // Close unused write end
+            dup2(pipefd[0], STDIN_FILENO);  // Redirect stdin to pipe
+            close(pipefd[0]); 
+            execvp(argv2, (char*)0);
+            printf("Failed execution");
+            exit(1);
+        } else {
+            close(pipefd[0]);
+            close(pipefd[1]);
+            int status;
+            waitpid(pid_a, &status, 0);
+            printf("In PARENT: successfully awaited child (pid = %ld)\n", pid_a);
+            waitpid(pid_b, &status, 0);
+            printf("In PARENT: successfully awaited child (pid = %ld)\n", pid_b);
+        }
+    }  
+    return 0;
+}
