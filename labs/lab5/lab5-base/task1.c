44dfab54ce27d97db7c5983a50cae44c2ef667cf
bchen17 <bchen17@binghamton.edu>
2025-03-18 16:18:26 -0400

    testing change2

--- labs/lab5/lab5-base/task1.c
+++ labs/lab5/lab5-base/task1.c
@@ -20,11 +20,11 @@ int main(void) {
         exit(EXIT_FAILURE);
     }
 
-    pid_t pid_a, pid_b = fork();
+    pid_t pid_a = fork();
 
     if (pid_a < 0){
         printf("did not work");
-    } else if (pid_a == 0 && pid_b != 0){
+    } else if (pid_a == 0){
         //in child
         printf("IN CHILD-1 (PID=%ld): executing command %s \n", getpid(), argv1[0]);
         close(pipefd[0]);               
@@ -33,23 +33,29 @@ int main(void) {
         execvp(argv1[0], argv1);
         printf("Failed execution");
         exit(1);
-    } else if (pid_b == 0 && pid_a != 0){
-        printf("IN CHILD-2 (PID=%ld): executing command %s \n", getpid(), argv2[0]);
-        close(pipefd[1]);               
-        dup2(pipefd[0], STDIN_FILENO); 
-        close(pipefd[0]); 
-        execvp(argv2[0], argv2);
-        printf("Failed execution");
-        exit(1);
     } else {
-    close(pipefd[0]);
-    close(pipefd[1]);
-    int status;
-    waitpid(pid_a, &status, 0);
-    printf("In PARENT (PID=%ld): successfully reaped child (pid = %ld)\n", getpid(), pid_a);
-    waitpid(pid_b, &status, 0);
-    printf("In PARENT (PID=%ld): successfully reaped child (pid = %ld)\n", getpid(), pid_a);
 
+        int status;
+        waitpid(pid_a, &status, 0);
+
+        pid_t pid_b = fork();
+
+        if (pid_b == 0) {
+            printf("IN CHILD-2 (PID=%ld): executing command %s \n", getpid(), argv2[0]);
+            close(pipefd[1]);               
+            dup2(pipefd[0], STDIN_FILENO); 
+            close(pipefd[0]); 
+            execvp(argv2[0], argv2);
+            printf("Failed execution");
+            exit(1);
+        } else {
+            close(pipefd[0]);
+            close(pipefd[1]);
+            waitpid(pid_b, &status, 0);
+            printf("In PARENT (PID=%ld): successfully reaped child (pid = %ld)\n", getpid(), pid_a);
+            printf("In PARENT (PID=%ld): successfully reaped child (pid = %ld)\n", getpid(), pid_b);
+
+        }
     }  
     return 0;
 }
