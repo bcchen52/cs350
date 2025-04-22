f177e1ed88547d4afb223fb19afe4baee17cd5f2
bchen17 <bchen17@binghamton.edu>
2025-04-22 14:02:58 -0400

    3.5

--- proj/proj3/proj3/base/sh.c
+++ proj/proj3/proj3/base/sh.c
@@ -224,18 +224,12 @@ main(void)
     if(buf[0] == 'h' && buf[1] == 'i' && buf[2] == 's' && buf[3] == 't'){
       if(buf[5] == 'p'){
         for(int i = 0; i<max; i++){
-          int new_location = location-i;
-          if(new_location < 0){
-            new_location = 10 - new_location;
-          }
+          int new_location = (location - 1 - i) % 10;
           printf(1, "Previous command %d: %s", i+1, tokens[new_location]);
         }
       } else if (buf[6] == '0'){ //assume this is 0
         if (buf[5] == '1'){
-          int new_location = location + 9;
-          if(new_location > 9){
-            new_location = new_location - 9;
-          }
+          int new_location = (location - 1) % 10;
           printf(1, "The %d command is at position %d good job", location, new_location);
         } else {
           printf(2, "Hist command format is wrong.");
@@ -243,10 +237,7 @@ main(void)
       } else if (buf[5] >= '0' && buf[5] <= '9'){
         int pos = buf[5] - '0';
         if(pos <= max){
-          int new_location = location - pos + 1;
-          if(new_location < 0){
-            new_location = 10 - new_location;
-          }
+          int new_location = (location - pos + 1) % 10;
           printf(1, "The %d command is at position %d good job", pos, new_location);
         } else {
           printf(1, "ur cooke");
@@ -266,11 +257,7 @@ main(void)
     if(max < 10){
       max++;
     };
-    if(location<9){
-      location++;
-    } else {
-      location = 0;
-    }
+    location = (location + 1) % 10;
     if (tokens[location]) {
       free(tokens[location]);  
     }
