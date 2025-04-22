147661088bf2780ad2cab806d56d1cf77d286e5a
bchen17 <bchen17@binghamton.edu>
2025-04-22 15:47:07 -0400

    finished proj3

--- proj/proj3/proj3/base_old/ps.c
+++
@@ -1,9 +0,0 @@
-#include "types.h"
-#include "stat.h"
-#include "user.h"
-#include "fcntl.h"
-
-int main(void){
-  cps();
-  exit();
-}
--- proj/proj3/proj3/base_old/types.h
+++
@@ -1,5 +0,0 @@
-typedef unsigned int   uint;
-typedef unsigned short ushort;
-typedef unsigned char  uchar;
-typedef unsigned int   xvpid_t;
-typedef uint pde_t;
